5b42b7db5844e648121839dc151c25916558f5ac
DanielJSan <danieljsan@usal.es>
2021-04-23 23:47:51 +0200

    funcion nPausas

--- cruce.c
+++ cruce.c
@@ -21,6 +21,7 @@
 #define FALSE   0
 #define NSEMAFOROS 5
 #define TAMMC 256
+#define MAXPROCESOS 127
 
 int sem;
 char *mc =  NULL;
@@ -55,6 +56,9 @@ void signalf(int numsem,int numsignal){
 
 void crearHijo();
 void cicloSem();
+void cruce();
+void nPausas(int n);
+
 //PRUEBA DE PUSH
 
  int main(int argc, char *argv[]) {
@@ -62,19 +66,22 @@ void cicloSem();
  	pid_t HIJOCICLOS;
  	int i;
  	struct sigaction manejadora;
+	struct sembuf sops[10];
  	
  	//CUANDO SE RECIBA UNA SIGINT SE FINALIZARA TODO DE FORMA ORDENADA
  	//Liberamos IPCS
  	manejadora.sa_handler = sig_action;
-       if (sigemptyset(&manejadora.sa_mask) != 0) return 0;
-       if (sigaddset(&manejadora.sa_mask, SIGINT) != 0) return 0;
-       manejadora.sa_flags = 0;
-       if (sigaction(SIGINT, &manejadora, NULL) != 0) return 0;
+    if (sigemptyset(&manejadora.sa_mask) != 0) return 0;
+    if (sigaddset(&manejadora.sa_mask, SIGINT) != 0) return 0;
+    manejadora.sa_flags = 0;
+    if (sigaction(SIGINT, &manejadora, NULL) != 0) return 0;
        
        
 	//1. COGER Y VERIFICAR INFO DE ARGUMENTOS
 	if(argc < 3 || argc > 3) { exit(-1); } 
 	//if(argv[1] > MAXPROC) { exit(-2); } 
+
+	if(atoi(argv[1]) > MAXPROCESOS) { perror("ERROR: Demasiados procesos, desbordamiento"); exit(-1); } 
 	
 	int nproc = atoi(argv[1]);
 	int velocidad = atoi(argv[2]);
@@ -83,10 +90,9 @@ void cicloSem();
 	if((sem = semget(IPC_PRIVATE, NSEMAFOROS, IPC_CREAT | 0600)) == -1) { printf("Error semget\n"); }; //Iniciar semaforo
 	//1 = numero de procesos que entran a la vez
 
-	
 	//if (semctl(sem, 1, SETVAL, 0) == -1) { printf("Error semctl\n"); }
 	 
-	
+
 	if((memid = shmget(IPC_PRIVATE, TAMMC, IPC_CREAT | 0600)) == -1) { printf("Error memid\n"); exit (-3); }; //Creacion de memoria compartida (Min: 256 bytes)
 	
 	mc = shmat(memid, NULL, 0); //Asociamos el puntero que devuelve shmat a una variable 
@@ -112,13 +118,14 @@ void cicloSem();
 	if(getpid()==PPADRE){
 		crearHijo();
 		if(getpid()!=PPADRE){
-			//cicloSem();
+			cicloSem();
 		
 		}
 	
 	}
+
 	if (semctl(sem, 0, SETVAL,nproc) == -1) { printf("Error semctl\n"); } //Operaciones del semaforo: Asigna nprocs de valor al semaforo 0
-	//while(1){
+	while(1){
 		if(getpid() == PPADRE) {
 			int tipo = CRUCE_nuevo_proceso();
 			/*int valor=semctl(sem, 0, GETVAL);
@@ -133,10 +140,11 @@ void cicloSem();
 			
 			posicionsig=CRUCE_inicio_coche();
 			sleep(3);
-			printf("%d %d\n",posicionsig.x,posicionsig.y);
+			//fprintf(stderr, "%d %d\t",posicionsig.x,posicionsig.y);
 			posicionsigsig=CRUCE_avanzar_coche(posicionsig);
-			printf("%d %d\t", posicionsigsig.x, posicionsigsig.y);
+			fprintf(stderr, "%d %d\t", posicionsigsig.x, posicionsigsig.y);
 			posicionsigsig=CRUCE_avanzar_coche(posicionsigsig);
+
 			/*while(posicionsigsig.y>=0){
 				//semaforo para entrar a la siguiente posicion
 				posicionsigsig = CRUCE_avanzar_coche(posicionsigsig);
@@ -180,7 +188,7 @@ void cicloSem();
 			*/
 			
 		}
-	//}
+	}
 
 	
 
@@ -202,28 +210,37 @@ void cicloSem();
  } 
  void cicloSem(){
  	while(1){
- 	
+		 //Para comprobar si hay un proceso en la zona critica del cruce hay que comprobar el valor del semaforo
  		if (semctl(sem, 4, SETVAL, 0) == -1) { printf("Error semctl\n"); }
- 		
+		 
+ 		waitf(4,1);
+
 		CRUCE_pon_semAforo(0,2);//SEM_C1 A VERDE
 		CRUCE_pon_semAforo(3,2);//SEM_P2 A VERDE
 		CRUCE_pon_semAforo(1,1);//SEM_C2 A ROJO
 		CRUCE_pon_semAforo(2,1);//SEM_P1 A ROJO
-		for(int j=0;j<6;j++){
-			pausa();
-		}
+
+		signalf(4,1);
+
+		nPausas(6);
+
+
 		//SEM_C1 A AMARILLO
-		CRUCE_pon_semAforo(0,3);//SEM_C1 A AMARILLO
-		pausa();
-		pausa();
+		if (semctl(sem, 4, SETVAL, 0) == -1) { printf("Error semctl\n"); }
+
+		cruce();
 		
+		waitf(4,1);
+
 		CRUCE_pon_semAforo(1,2);//SEM_C2 A VERDE
 		CRUCE_pon_semAforo(0,1);//SEM_C1 A ROJO
 		CRUCE_pon_semAforo(3,1);//SEM_P2 A ROJO			
 		CRUCE_pon_semAforo(2,1);//SEM_P1 A ROJO
-		for(int j=0;j<9;j++){
-			pausa();
-		}
+
+		nPausas(9);
+
+		signalf(4,1);
+
 		//SEM_C2 A AMARILLO
 		CRUCE_pon_semAforo(1,3);//SEM_C2 A AMARILLO
 		pausa();
@@ -233,10 +250,17 @@ void cicloSem();
 		CRUCE_pon_semAforo(1,1);//SEM_C2 A ROJO
 		CRUCE_pon_semAforo(0,1);//SEM_C1 A ROJO
 		CRUCE_pon_semAforo(3,1);//SEM_P2 A ROJO
-		for(int j=0;j<12;j++){
-			pausa();
-		}	
+
+		nPausas(12);
+		
+			
 	}
+}
+
+void cruce() {
+	CRUCE_pon_semAforo(0,3);//SEM_C1 A AMARILLO
+	pausa();
+	pausa();
 }
 
 //Mientras haya cambio de semaforos bloqueamos la entrada al cruce de todos los procesos
@@ -251,3 +275,9 @@ void cicloSem();
  			exit(-1);
  	}
  } 
+
+void nPausas(int n) {
+	for(int j = 0; j < n; j++){
+			pausa();
+		}
+}
